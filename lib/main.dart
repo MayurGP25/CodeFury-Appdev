c3455d974b22d50d904c96fab361faf5b9fd0186
MayurGP25 <114596885+MayurGP25@users.noreply.github.com>
2023-09-24 00:28:53 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,18 @@
+import 'package:app/pages/home.dart';
+import 'package:app/pages/onboarding.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: OnboardingPage(),
+    );
+  }
+}
 
